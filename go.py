431518b86175c0ae83180bf0a8ea2baee9a6822b
djksa <nopumper@gmail.com>
2018-12-08 12:27:32 +1300

    fixed go function - now grabs pokemon of correct location

--- go.py
+++ go.py
@@ -6,28 +6,33 @@ import random
 from discord.ext import commands
 
 """cog for exploring"""
-
+# /TODO extend ctx of getpokemon cog to find specific pokemon then add here instead
+# of random encounter being long
+#so far it only generates a number but i have to get the actual name otherwise the location will NOT matter
 class Go():
     def __init__(self, bot):
         self.bot = bot
 
-
-
-
     def random_encounter(self, poke_encounters):
+        #returns int
         poke_index = random.randint(0, len(poke_encounters))
-        r = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(poke_index) + "/")
-        poke_name = r.json()["name"]
+        return poke_index
+    
+    def grab_pokemon(self, random_encounter, location):
+        #returns str
+        r = requests.get("https://pokeapi.co/api/v2/location-area/" + location + "/")
+        poke_name = r.json()["pokemon_encounters"][random_encounter]["pokemon"]["name"]
         return poke_name
+
 
     @commands.command(pass_context=True)
     async def go(self, ctx, location: str):
         """takes you to a location"""
         r = requests.get("https://pokeapi.co/api/v2/location-area/" + location + "/")
         poke_encounters = r.json()["pokemon_encounters"]
-        pokemon_caught = self.random_encounter(poke_encounters)
-        poke_img = "http://play.pokemonshowdown.com/sprites/xyani/" + pokemon_caught + ".gif"
-        await self.bot.send_message(ctx.message.channel, pokemon_caught, tts=False)
+        poke_name = self.grab_pokemon(self.random_encounter(poke_encounters), location)
+        poke_img = "http://play.pokemonshowdown.com/sprites/xyani/" + poke_name + ".gif"
+        await self.bot.send_message(ctx.message.channel, poke_name, tts=True)
         await self.bot.say(poke_img)
         
 def setup(bot):
