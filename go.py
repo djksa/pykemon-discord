4cee2e1ffbc78014770a176bebd3253d98eaf848
djksa <nopumper@gmail.com>
2018-12-08 20:49:35 +1300

    added async locations func which sends 10 msgs worth of locations

--- go.py
+++ go.py
@@ -34,6 +34,26 @@ class Go():
         poke_img = "http://play.pokemonshowdown.com/sprites/xyani/" + poke_name + ".gif"
         await self.bot.send_message(ctx.message.channel, poke_name, tts=True)
         await self.bot.say(poke_img)
-        
+
+    @commands.command(pass_context=True)
+    async def locations(self):
+        r = requests.get("https://pokeapi.co/api/v2/location-area/")
+        locations_list = r.json()["results"]
+        locations_str = ""
+        for i in range(len(locations_list)): 
+            locations_str += (locations_list[i]["name"] + " | ")
+        locations_str_tenth = int(len(locations_str) / 10) #NEED TO FIX LOCATION NAMES BEING CUT ON LAST FEW CHARS OF MESSAGE
+        current_index = 0
+        for i in range(10):
+            if i != 9:
+                print("```\nLOCATIONS PAGE : " + str(i + 1) + "\n" + locations_str[current_index : current_index + locations_str_tenth] + "\n```")
+                await self.bot.say("```\nLOCATIONS PAGE : " + str(i + 1) + "\n" + locations_str[current_index : current_index + locations_str_tenth] + "\n```")
+                current_index += locations_str_tenth
+            else:
+                print("```\nLOCATIONS PAGE : " + str(i + 1) + "\n" + locations_str[current_index :] + "\n```")
+                await self.bot.say("```\nLOCATIONS PAGE : " + str(i + 1) + "\n" + locations_str[current_index :] + "\n```")
+
 def setup(bot):
     bot.add_cog(Go(bot))  
+
+
